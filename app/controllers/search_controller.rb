9090238f7189049b24735cbee28770e1b2555d75
Peter Jacobs <peter.jacobs@ambassador-enterprises.com>
2016-11-07 14:51:31 -0500

    search form and minimal styles

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,22 @@
 class SearchController < ApplicationController
   def index
+    # if logged in, load the current user
     if session[:user_id]
       @user = User.find_by_id(session[:user_id])
-    elsif Rails.env.developer?
-      redirect_to user_developer_authorize_url
-    else
-      # redirect_to user_github_omniauth_authorize_url
     end
+
+    # search query, if present
+    @query = params[:query] || ''
+    @page = params[:page] || 0
+
+    if @query
+      @results = get_search_results(@query, @page)
+    end
+  end
+
+  private
+
+  def get_search_results(query, page=0)
+    return []
   end
 end
