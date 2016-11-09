a3658f550f411e11c0ec8098c12860d5a816b648
Peter Jacobs <peter.jacobs@ambassador-enterprises.com>
2016-11-09 16:51:47 -0500

    tidy up pagination a little bit

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -5,4 +5,9 @@ module SearchHelper
                    :page => hash[:page] || params[:page],
                    :per_page => hash[:per_page] || params[:per_page]}
   end
+
+  # same as above, but just a shortcut to link to a specific page of results
+  def link_to_search_page(name, page)
+    link_to_search_with name, :page => page
+  end
 end
